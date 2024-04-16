57fa179def6d8f587123a5a66739fbff89c482fa
Raphaël Tournafond <32420506+raphaeltournafond@users.noreply.github.com>
2024-04-16 21:13:17 +0200

    refactor: update accounts app name

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class UserAppConfig(AppConfig):
+class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'user_app'
+    name = 'accounts'
