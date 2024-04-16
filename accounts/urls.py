7ea85fe1a351798517bf0b952adf7bc1e2965148
Raphaël Tournafond <32420506+raphaeltournafond@users.noreply.github.com>
2024-04-17 00:17:32 +0200

    feat: add users listing

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Register, Login
+from .views import Register, Login, UserList
 
 urlpatterns = [
     path('register/', Register.as_view(), name='register'),
     path('login/', Login.as_view(), name='login'),
+    path('users/', UserList.as_view(), name='list_users'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,11 @@
+from django.contrib.auth.models import User
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAdminUser
 from .serializers import UserSerializer, UserRegisterSerializer, UserLoginSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 class Register(APIView):
     """
@@ -27,3 +30,21 @@ class Login(TokenObtainPairView):
             response = super(Login, self).post(request, *args, **kwargs)
             return response
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class UserList(APIView):
+    """
+    View to list all users in the system.
+
+    * Requires token authentication.
+    * Only admin users are able to access this view.
+    """
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAdminUser]
+
+    def get(self, request, format=None):
+        """
+        Return a list of all users.
+        """
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
