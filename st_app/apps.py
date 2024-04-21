044ca2847e355a471608179548682182f5443c6e
Raphaël Tournafond <32420506+raphaeltournafond@users.noreply.github.com>
2024-04-21 17:41:20 +0200

    build: connect session model

--- st_app/apps.py
+++ st_app/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class SmartTrackerAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'smart_tracker_app'
+    name = 'st_app'
--- st_app/models.py
+++ st_app/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Session(models.Model):
+
+    class Meta:
+        app_label = 'st_app'
+        
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
