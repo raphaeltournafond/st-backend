377ff3341bbd56e7c73053f9cf946235ee2f1083
Raphaël Tournafond <32420506+raphaeltournafond@users.noreply.github.com>
2024-04-21 15:25:50 +0200

    feat: add base for Session model

--- smart_tracker_app/models.py
+++ smart_tracker_app/models.py
@@ -1,3 +1,10 @@
+import json
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Session(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    data = models.TextField()
+
