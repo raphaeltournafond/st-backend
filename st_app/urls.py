255d5f2f9fc82aa31a51a2eb072612c4406a5a41
Raphaël Tournafond <32420506+raphaeltournafond@users.noreply.github.com>
2024-04-26 14:19:58 +0200

    fix: typo

--- st_app/urls.py
+++ st_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import SessionCreate, SessionDetail
 
 urlpatterns = [
-    path('sessions/', SessionCreate.as_view(), name='session-create'),
-    path('sessions/<int:session_id>/', SessionDetail.as_view(), name='session-detail'),
+    path('sessions/', SessionCreate.as_view(), name='session_create'),
+    path('sessions/<int:session_id>/', SessionDetail.as_view(), name='session_detail'),
 ]
