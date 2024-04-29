4f9a0b4f613dc5b13a3c5b10f9f85b3ea8983db1
Raphaël Tournafond <32420506+raphaeltournafond@users.noreply.github.com>
2024-04-29 14:18:17 +0200

    feat: add health check route

--- smart_tracker/urls.py
+++ smart_tracker/urls.py
@@ -22,6 +22,8 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView,
 )
 
+from .views import health_check
+
 urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('st/', include('st_app.urls')),
@@ -29,4 +31,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/misc/check/', health_check, name="health_check"),
 ]
